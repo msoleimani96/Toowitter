11126580c669a89cd17096dac8e1f67c188aecbe
Matin Soleimani <matinsoleimani2006@gmail.com>
2024-07-16 20:48:49 +0330

    models designed and implemented.

--- toowitter/toowitter_api/models.py
+++ toowitter/toowitter_api/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=30)
+    username = models.CharField(max_length=50)
+    email = models.EmailField()
+    password = models.CharField(max_length=40)
+    bio = models.CharField(max_length=128)
+
+
+class Tweet(models.Model):
+    text = models.CharField(max_length=512)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class View(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    tweet = models.ForeignKey(Tweet, on_delete=models.CASCADE)
+
+
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    tweet = models.ForeignKey(Tweet, on_delete=models.CASCADE)
