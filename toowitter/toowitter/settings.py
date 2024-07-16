584ce2ef3109c4d79dfcd9690224f10c2248ade6
Matin Soleimani <matinsoleimani2006@gmail.com>
2024-07-16 17:50:18 +0330

    Some structural changes.

--- toowitter/toowitter/settings.py
+++ toowitter/toowitter/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'toowitter_api'
 ]
 
 MIDDLEWARE = [
--- toowitter/toowitter/urls.py
+++ toowitter/toowitter/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from toowitter_api import urls as toowitter_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(toowitter_urls)),
+    path('api-auth/', include('rest_framework.urls')),
 ]
