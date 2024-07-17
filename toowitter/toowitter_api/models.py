7e163e2429c08f14f750a84fa7bed6e1f04390cc
Matin Soleimani <matinsoleimani2006@gmail.com>
2024-07-17 21:21:46 +0330

    Some model changes

--- toowitter/toowitter_api/models.py
+++ toowitter/toowitter_api/models.py
@@ -10,12 +10,18 @@ class User(models.Model):
     password = models.CharField(max_length=40)
     bio = models.CharField(max_length=128)
 
+    def __str__(self):
+        return self.email
+
 
 class Tweet(models.Model):
     id = models.AutoField(primary_key=True)
     text = models.CharField(max_length=512)
     date = models.DateField(default=datetime.date.today)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.text[:20]
 
 
 class View(models.Model):
