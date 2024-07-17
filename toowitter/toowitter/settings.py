194f2417e16eba849540cfc02a1d1ea5140ebf63
Matin Soleimani <matinsoleimani2006@gmail.com>
2024-07-17 16:50:49 +0330

    gitignore updated and some settings changed.

--- toowitter/toowitter/settings.py
+++ toowitter/toowitter/settings.py
@@ -77,8 +77,8 @@ WSGI_APPLICATION = 'toowitter.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {'read_default_file': 'db.cnf'}
     }
 }
 
