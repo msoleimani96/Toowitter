72cd8cd1f79eb5cd4d1c00d4b71105bb69548bfc
Matin Soleimani <matinsoleimani2006@gmail.com>
2024-07-17 21:21:58 +0330

    views implemented.

--- toowitter/toowitter_api/views.py
+++ toowitter/toowitter_api/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework import permissions, status
+from rest_framework.response import Response
+from serializers import TweetSerializer, UserSerializer
+from .models import *
+from django.core.paginator import Paginator
 
-# Create your views here.
+
+class TweetApiView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+        tweets = Tweet.objects.filter(user=user)
+        serializer = TweetSerializer(tweets, many=True)
+        paginated_data = Paginator(serializer.data, 10)
+        return Response(paginated_data)
